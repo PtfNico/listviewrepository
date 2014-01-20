ce7380255d7fec9a6e3da51568496976dbdea87a
Nicolas Martinez <nicolasm@bpoplataforma.com>
2014-01-19 23:05:27 -0200

    primer commit yay!

---
+++ listviewtest/src/com/example/listviewtest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.listviewtest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		final String[] datos = new String[]{"Penny", "Java", "Tadeo"};
+		ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos);
+		ListView lstopciones = (ListView) findViewById(R.id.lstopciones);
+		lstopciones.setAdapter(adaptador);
+		
+		lstopciones.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				ListAdapter la = (ListAdapter) arg0.getAdapter();
+				Toast.makeText(arg1.getContext(), la.getItem(arg2).toString(), Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
